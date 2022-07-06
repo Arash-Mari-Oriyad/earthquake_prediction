70d705c962ba7cac6d425565ac9b9ff90cfd1a13
Arash Mari Oriyad <arashmarioriyad@gamil.com>
2022-07-06 14:24:28 +0430

    add main experiment with grouping plan experimnet

---
+++ experiments/main_experiment_with_grouping_plan/configurations.py
@@ -0,0 +1,19 @@
+HISTORY_LENGTHS = [i for i in range(1, 13)]
+FORECAST_HORIZON = 1
+START_YEAR, END_YEAR = 1966, 2016
+N_HORIZONTAL, N_VERTICAL = 3, 3
+GROUPING_PLAN = True
+GROUPS = [[1, 2, 5, 6], [4, 7, 8, 9], [3]]
+
+VALIDATION_SIZE = 0.3
+TESTING_SIZE = 0.3
+
+MODELS = ["knn", "glm"]
+
+HISTORICAL_DATA_BASE_ADDRESS = f"../../data/historical_data/{START_YEAR}_{END_YEAR}/{N_HORIZONTAL}_{N_VERTICAL}/" \
+                               f"{'with_grouping_plan' if GROUPING_PLAN else 'without_grouping_plan'}"
+RESULTS_BASE_ADDRESS = "results"
+VALIDATION_PREDICTIONS_BASE_ADDRESS = "prediction/validation process"
+TESTING_PREDICTIONS_BASE_ADDRESS = "prediction/test process"
+
+INF = pow(10, 12)
---
+++ experiments/main_experiment_with_grouping_plan/execute_experiment.py
@@ -0,0 +1,45 @@
+import os
+import shutil
+
+import stpredict
+
+from configurations import HISTORICAL_DATA_BASE_ADDRESS, FORECAST_HORIZON, HISTORY_LENGTHS, \
+    MODELS, RESULTS_BASE_ADDRESS, VALIDATION_SIZE, TESTING_SIZE, GROUPS
+
+
+def main():
+    for group in GROUPS:
+        input_data_addresses = \
+            [f"{HISTORICAL_DATA_BASE_ADDRESS}/{'_'.join([str(sub_region_id) for sub_region_id in group])}" \
+             f"/{FORECAST_HORIZON}/historical_data h={h}.csv"
+             for h in HISTORY_LENGTHS]
+        for model in MODELS:
+            stpredict.whole_as_one(data=input_data_addresses,
+                                   forecast_horizon=FORECAST_HORIZON,
+                                   feature_sets={'feature': 'mRMR'},
+                                   model_type='classification',
+                                   models=[model],
+                                   splitting_type='training-validation',
+                                   instance_testing_size=TESTING_SIZE,
+                                   instance_validation_size=VALIDATION_SIZE,
+                                   performance_benchmark='AUC',
+                                   performance_measures=['AUC', 'likelihood', 'AUPR'],
+                                   validation_performance_report=True,
+                                   testing_performance_report=True,
+                                   save_ranked_features=True,
+                                   save_predictions=True,
+                                   verbose=1)
+            results_address = f"{RESULTS_BASE_ADDRESS}/{'_'.join([str(sub_region_id) for sub_region_id in group])}" \
+                              f"/{model}/"
+            if os.path.exists(results_address):
+                shutil.rmtree(results_address)
+            os.makedirs(results_address)
+            shutil.move("prediction", results_address)
+            shutil.move("performance", results_address)
+            shutil.move("plots", results_address)
+            shutil.move("ranked features", results_address)
+    return
+
+
+if __name__ == "__main__":
+    main()
